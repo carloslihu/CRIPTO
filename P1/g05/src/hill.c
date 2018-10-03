2260f3e226751904bf87d0d85df35a2ec30b6127
David <david.lopez.4abc@gmail.com>
2018-10-03 18:56:40 +0200

    Hill parsea
    pues eso... que es hill ... y parsea ... cosas

--- P1/g05/src/hill.c
+++ P1/g05/src/hill.c
@@ -62,11 +62,11 @@ int main(int argc, char **argv) {
     char entrada[256];
     char cadena[256];
     int long_index = 0; //, retorno = 0;
-    char opt, simbolo_in, simbolo_out;
-    FILE *fIn, *fOut, *fK;
+    char opt, simbolo_in, simbolo_out, fill='W';
+    FILE *fIn=NULL, *fOut=NULL, *fK=NULL, *fAux=NULL;
     int cifrar = -1;
     int matrix[3][3] = { 0 };
-    int m=0, n=0, count=0, i=0, j=0;
+    int m=0, n=0, count=0, i=0, j=0, det=0;
 
     if (argc > 1) {
         strncpy(entrada, argv[1], 256);
@@ -102,6 +102,10 @@ int main(int argc, char **argv) {
 
             case '2':
                 n=atoi(optarg);
+                if(n<1 || n>3){
+                    printf("El tamano n de matriz debe estar en el rango [1,3]\n");
+                    exit(-1);
+                }
                 break;
 
             case '3':
@@ -135,11 +139,6 @@ int main(int argc, char **argv) {
     }
 
 
-/*    if (mpz_invert(inv, a, m) == 0) {
-        printf("La clave no determina una función afín inyectiva\n");
-        exit(-1);
-    }*/
-
 
     /*leer fichero claves*/
     for(i = 0; i < n; i++){
@@ -153,22 +152,72 @@ int main(int argc, char **argv) {
     fclose(fK);
 
 
+    /*Calculo de determinante*/
+    if(n==1){
+        det = matrix[0][0];
+    }
+    else if(n==2){
+        det = matrix[0][0]*matrix[1][1] - matrix[0][1]-matrix[1][0];
+    }
+    else if(n==3){
+        det = matrix[0][0]*matrix[1][1]*matrix[2][2] + matrix[1][0]*matrix[2][1]*matrix[0][2] + matrix[0][1]*matrix[1][2]*matrix[2][0] - matrix[0][2]*matrix[1][1]*matrix[2][0] - matrix[2][1]*matrix[1][2]*matrix[0][0] - matrix[1][0]*matrix[0][1]*matrix[2][2];;
+    }
+    else{
+        printf("La matriz de claves no puede superar 3x3\n");
+        exit(-1);   
+    }
 
+    if(det==0){
+        printf("La clave no puede tener determinante 0 \n");
+        exit(-1);
+    }
 
+    printf("El determinante es %d\n", det);
+
+    if (mcd(m, det) != 1) {
+        printf("La clave no determina una función inyectiva\n");
+        exit(-1);
+    }
 
     /*crear entrada estandar*/
-/*    if (!fIn) {
+    if (!fIn) {
         printf("Leyendo entrada estandar \n");
         fgets(cadena, 256, stdin);
         fIn = fopen("teclado.txt", "w");
         fwrite(cadena, 1, strlen(cadena), fIn);
         fclose(fIn);
         fIn = fopen("teclado.txt", "r");
-    }*/
+    }
 
-    /*leer fichero entrada o estandar*/
+    /*Limpiar texto de entrada con letras A-Z*/
+    if (fIn) {
+        fAux= fopen("auxiliar.txt", "w");
+        if (fAux) {
+            while (fscanf(fIn, "%c", &simbolo_in) != EOF) {
+                /*convertir a mayusculas*/
+                if ('a' <= simbolo_in && simbolo_in <= 'z') {
+                    simbolo_in -= ('a' - 'A');
+                }
+                if ('A' <= simbolo_in && simbolo_in <= 'Z') {
+                    /*escribir fichero auxiliar de entrada*/
+                    printf("Anadiendo a auxiliar \n");    
+                    fwrite(&simbolo_in, 1, 1, fAux);
+                    count++;
+                }
+            }
+            /*rellenar texto para hacerlo modulo N*/
+            count = count%n;
+            for(i=0; i<count; i++) fwrite(&fill, 1, 1, fAux);
+            fclose(fAux);
+            fIn = fopen("auxiliar.txt", "r");
+        }
+    }
+
+
+
+      /*leer fichero entrada o estandar*/
     // if (fIn) {
-    //     while (fscanf(fIn, "%c", &simbolo_in) != EOF) {
+    //     while (fread(fIn, "%c", &simbolo_in) != EOF) {
     //         /*convertir a mayusculas*/
     //         if ('a' <= simbolo_in && simbolo_in <= 'z') {
     //             simbolo_in -= ('a' - 'A');
@@ -181,19 +230,13 @@ int main(int argc, char **argv) {
 
     //             /*Cifrar*/
     //             if (cifrar == 1) {
-    //                 mpz_mul(aux2, a, aux);
-    //                 mpz_mod(aux2, aux2, m);
-    //                 mpz_add(aux2, aux2, b);
-    //                 mpz_mod(aux2, aux2, m);
+
     //             }/*Descifrar*/
     //             else {
-    //                 /*simbolo_out = (inv*(simbolo_in - b)) % m;*/
-    //                 mpz_sub(aux2, aux, b);
-    //                 mpz_mul(aux2, aux2, inv);
-    //                 mpz_mod(aux2, aux2, m);
+                    
     //             }
 
-    //             /*convertir a double o int y sumar 65, codigo de la primera letra A*/
+    //             convertir a double o int y sumar 65, codigo de la primera letra A
     //             simbolo_out = mpz_get_d(aux2);
     //             simbolo_out += 65;
     //         } else {
@@ -211,8 +254,21 @@ int main(int argc, char **argv) {
 
     // }
 
-/*    if (fIn) fclose(fIn);
-    if (fOut) fclose(fOut);*/
+
+    // if(fOut){
+    //     printf("Hay fOut\n"); 
+    // }
+    // else{
+    //     printf("No hay fOut\n"); 
+    // }
+
+
+
+
+
+
+    if (fIn) fclose(fIn);
+    if (fOut) fclose(fOut);
 
     return 0;
 
