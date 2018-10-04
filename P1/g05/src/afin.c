930ea074c6cb627a06edd04104eafa24fed17ec6
Carlos Li Hu <carloslihu96@gmail.com>
2018-10-04 21:55:06 +0200

    implementando mcd en gmp, euclides extendido sigue fallando

--- P1/g05/src/afin.c
+++ P1/g05/src/afin.c
@@ -7,7 +7,7 @@ Autores: Carlos Li Hu y David López Ramos
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> 
+#include <math.h>
 #include <string.h>
 #include <time.h>
 #include <getopt.h>
@@ -16,19 +16,28 @@ Autores: Carlos Li Hu y David López Ramos
 /*Definicion de constantes *************************************************/
 
 /**
- * @brief Calcula el maximo comun divisor
+ * @brief Calcula el maximo comun divisor usando gmp
  *
- * @param primer entero
- * @param segundo entero
+ * @param resultado de la operación
+ * @param primer operando
+ * @param segundo operando
  *
- * @return el maximo comun divisor
  */
-int mcd(int a, int b) {
-    if (a == 0)
-        return b;
-    return mcd(b % a, a);
+void gmp_mcd(mpz_t rop, mpz_t op1, mpz_t op2) {
+    mpz_t a, b, r;
+    mpz_inits(a, b, r, NULL);
+    mpz_set(a,op1);
+    mpz_set(b,op2);
+
+    if (mpz_cmp_d (a, 0) == 0){
+      mpz_set(rop,b);
+      mpz_clears(a, b, r, NULL);
+      return;
+    }
+    mpz_mod (r, b, a);
+    gmp_mcd(rop, r, a);
+    mpz_clears(a, b, r, NULL);
 }
-// Se guarda en a el inverso de a modulo m en x
 
 /**
  * @brief Realiza la descomposición de Euclides Extendida.
@@ -41,7 +50,31 @@ int mcd(int a, int b) {
  *
  * @return el maximo comun divisor
  */
-int mcdExtended(int a, int m, int *x, int *y) {
+void gmp_mcdext (mpz_t g, mpz_t s, mpz_t t, const mpz_t a1, const mpz_t m1){
+  mpz_t a, m, x, y,x1,y1,aux;
+  mpz_inits(a,m,x,y,x1,y1,aux,NULL);
+  mpz_set(a,a1);
+  mpz_set(m,m1);
+
+
+  if (mpz_cmp_d (a, 0) == 0){
+    mpz_set_d(x,0);
+    mpz_set_d(y,1);
+    return;
+  }
+  mpz_mod (aux, m, a);
+  gmp_mcdext (g,x1,y1,aux,a);
+
+  mpz_cdiv_q (aux, m, a);
+  mpz_mul (aux, aux, x1);
+  mpz_sub(aux, y1, aux);
+
+  mpz_set(x,aux);
+  mpz_set(y,x1);
+  mpz_clears(a,m,x,y,x1,y1,aux,NULL);
+}
+
+/*int mcdExtended(int a, int m, int *x, int *y) {
     if (a == 0) {
         *x = 0;
         *y = 1;
@@ -55,7 +88,7 @@ int mcdExtended(int a, int m, int *x, int *y) {
     *y = x1;
 
     return mcd;
-}
+}*/
 
 /* PROGRAMA PRINCIPAL */
 int main(int argc, char **argv) {
@@ -63,7 +96,7 @@ int main(int argc, char **argv) {
     char cadena[256];
     int long_index = 0; //, retorno = 0;
     char opt, simbolo_in, simbolo_out;
-    mpz_t a, b, m, inv, aux, aux2;
+    mpz_t a, b, m, inv, mcd,t, aux, aux2;
     FILE *fIn, *fOut;
     int cifrar = -1;
 
@@ -85,7 +118,7 @@ int main(int argc, char **argv) {
         {0, 0, 0, 0}
     };
 
-    mpz_inits(a, b, m, inv, aux, aux2, NULL);
+    mpz_inits(a, b, m, inv,t, mcd, aux, aux2, NULL);
     while ((opt = getopt_long_only(argc, argv, "c:d:1:2:3:4:5", options, &long_index)) != -1) {
         switch (opt) {
             case 'c':
@@ -133,6 +166,13 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
+    gmp_mcd(mcd, a, m);
+    gmp_printf ("El resultado del mcd es %Zd\n", mcd);
+    gmp_mcdext (mcd, inv, t, a,m);
+    gmp_printf ("Los resultados del mcdext son: %Zd %Zd %Zd\n", mcd,inv,t);
+    mpz_gcdext (mcd, inv, t,a, m);
+    gmp_printf ("Los resultados del mcdext deberian ser: %Zd %Zd %Zd\n", mcd,inv,t);
+    return 0;
 
     if (mpz_invert(inv, a, m) == 0) {
         printf("La clave no determina una función afín inyectiva\n");
@@ -203,4 +243,3 @@ int main(int argc, char **argv) {
 
 
 }
-
