1f5ce8157cc8e780ffb513eb85fab7b3b24f6494
Carlos Li Hu <carloslihu96@gmail.com>
2018-10-19 19:24:02 +0200

    kasiski recorre correctamente, faltan detalles
    ignoro superposiciones?

--- P1/g05/src/kasiski.c
+++ P1/g05/src/kasiski.c
@@ -5,15 +5,18 @@ Autores: Carlos Li Hu y David López Ramos
 2018 EPS-UAM
  ***************************************************************************/
 #include "../includes/utils.h"
+#define TAM 1000000
 
 int main(int argc, char **argv) {
     char entrada[256];
     int long_index = 0; //, retorno = 0;
     char opt;
     FILE *fIn = NULL, *fOut = NULL;
-    int l;
+    int l, Clen;
+    int i, j, k, i0 = -1, k0 = 0;
+    char C[TAM], R[TAM];
 
-    /*if (argc > 1) {
+    if (argc > 1) {
         strncpy(entrada, argv[1], 256);
     } else {
         printf("Ejecucion: %s {-l Ngrama} [-i filein] [-o fileout]\n", argv[0]);
@@ -51,7 +54,22 @@ int main(int argc, char **argv) {
                 exit(-1);
                 break;
         }
-    }*/
-
+    }
+    fscanf(fIn, "%s", C);
+    Clen = strlen(C) + 1;
+    for (i = 0; i < Clen; i++) {
+        for (j = i + 1; j < Clen; j++) {
+            for (k = 0; C[i + k] == C[j + k]; k++) {
+                R[k] = C[i + k];
+            }
+            R[k] = '\0';
+            //printf("%s\n", R);
+            if (k >= l) {
+                printf("%s\tposiciones: %d  y %d con distancia %d\n", R, i, j, j - i);
+            }
+        }
+    }
+    if (fIn) fclose(fIn);
+    if (fOut) fclose(fOut);
     return 0;
 }
