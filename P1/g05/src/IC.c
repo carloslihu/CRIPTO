f294632070138c6d9659a3e40ee611e424823bd3
Carlos Li Hu <carloslihu96@gmail.com>
2018-10-20 19:41:48 +0200

    Al fin funciona IC :D

--- P1/g05/src/IC.c
+++ P1/g05/src/IC.c
@@ -15,9 +15,9 @@ Autores: Carlos Li Hu y David López Ramos
 int main(int argc, char **argv) {
     char entrada[256];
     int long_index = 0;
-    char opt, simbolo_in, **y = NULL;
+    char opt, simbolo_in;
     FILE *fIn = NULL, *fOut = NULL;
-    int l = 0, i = 0, j = 0, k = 0;
+    int l = 0, i = 0, j = 0, k = 0, n = 0;
     double IC_c = 0, IC_i = 0;
     double f_c[M] = {11.96, 0.92, 2.92, 6.87, 16.78, 0.52, 0.73, 0.89, 4.15,
         0.30, 0.0, 8.37, 2.12, 7.01, 8.69, 2.77, 1.53, 4.94, 7.88, 3.31, 4.80,
@@ -25,7 +25,9 @@ int main(int argc, char **argv) {
     double f_i[M] = {8.04, 1.54, 3.06, 3.99, 12.51, 2.30, 1.96, 5.49, 7.26,
         0.16, 0.67, 4.14, 2.53, 7.09, 7.60, 2.00, 0.11, 6.12, 6.54, 9.25, 2.71,
         0.99, 1.92, 0.19, 1.73, 0.19};
-    double *IC = NULL;
+    double *IC = NULL, **Mg = NULL;
+    int max = 0;
+    char imax = 0;
     int **f = NULL;
     if (argc > 1) {
         strncpy(entrada, argv[1], 256);
@@ -75,44 +77,71 @@ int main(int argc, char **argv) {
     printf("IC_c: %lf\nIC_i: %lf\n", IC_c, IC_i);
 
     /*Inicializamos variables*/
-    /*Esta variable la puedo borrar, pero de momento se queda para depurar */
-    y = (char**) calloc(l, sizeof (char*));
     f = (int**) calloc(l, sizeof (int*));
     IC = (double*) calloc(l, sizeof (double));
+    Mg = (double**) calloc(l, sizeof (double*));
     for (i = 0; i < l; i++) {
-        y[i] = (char*) calloc(TAM, sizeof (char));
         f[i] = (int*) calloc(M, sizeof (int));
+        Mg[i] = (double*) calloc(M, sizeof (double));
         for (j = 0; j < M; j++) {
             f[i][j] = 0;
+            Mg[i][j] = 0;
         }
         IC[i] = 0;
     }
     /*Leemos letras uno a uno y actualizamos las frecuencias absolutas */
+    /*f[i][k] guardará para el vector i, la frecuencia absoluta del caracter k */
     for (i = 0, j = 0; fscanf(fIn, "%c", &simbolo_in) != EOF; i++) {
         if (i == l) {
             i = 0;
             j++;
         }
-        y[i][j] = simbolo_in - 65;
-        f[i][(int) y[i][j]]++;
-        //printf("[%d,%d]: %d\t", i, j, y[i][j]);
+        f[i][(int) (simbolo_in - 65)]++;
     }
     for (i = 0; i < l; i++) {
         for (k = 0; k < M; k++) {
             IC[i] += f[i][k] * (f[i][k] - 1);
         }
+        /*En j guardamos el tamaño de cada vector */
         IC[i] /= j * (j - 1);
         //printf("IC_%d: %lf\n", i, IC[i]);
         fprintf(fOut, "IC_%d: %lf\n", i, IC[i]);
     }
+    /* para cada coordenada Ki de la clave */
+    for (i = 0; i < l; i++) {
+        /* Si desplazamos -n las frecuencias */
+        fprintf(fOut, "Valores K_%d\n", i);
+        max = 0;
+        imax = 0;
+        for (n = 0; n < M; n++) {
+            /* Calculamos los IC */
+            for (k = 0; k < M; k++) {
+                if ((k + n) < M) {
+                    Mg[i][n] += f_c[k] * f[i][k + n];
+                } else {
+                    Mg[i][n] += f_c[k] * f[i][(k + n) - M];
+                }
+            }
+            if (max < Mg[i][n]) {
+                max = Mg[i][n];
+                imax = n;
+            }
+            Mg[i][n] = Mg[i][n] / (j * 100);
+
+            fprintf(fOut, "%lf\t", Mg[i][n]);
+        }
+
+
+        fprintf(fOut, "\nLa posible clave para K%d es %c\n\n", i, imax + 65);
+    }
 
 
     for (i = 0; i < l; i++) {
-        free(y[i]);
         free(f[i]);
+        free(Mg[i]);
     }
-    free(y);
     free(f);
+    free(Mg);
     free(IC);
     if (fIn) fclose(fIn);
     if (fOut) fclose(fOut);
